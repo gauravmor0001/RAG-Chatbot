38fd0ae3fbafdf1b6d149dc52b5ff56be7de4b50
gauravmor0001 <gauravmor0001@gmail.com>
2026-02-28 12:09:28 +0530

    more restructuring

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ To start the project in your own system:
 ->activate venv: .venv/Scripts/Activate
 ->go inside memory folder and activate docker:docker compose up -d(start docker app in background)
 ->activate server:python server.py , while remaining in memory folder.
+
+
+#notes:
+->with __init__.py we can import easily, it tells that this is a package.
--- backend/tools.py
+++ backend/tools.py
@@ -4,10 +4,16 @@ from langchain_community.tools import TavilySearchResults
 import wikipedia
 from langchain_qdrant import QdrantVectorStore
 from langchain_huggingface import HuggingFaceEmbeddings
+from sentence_transformers import CrossEncoder 
+from langchain_core.runnables import RunnableConfig #secure back channel
+from qdrant_client.http import models #we can not simply say filter using user_id to qdrant, so to make the format of the filter we require this.
+
 
 embedding_model = HuggingFaceEmbeddings(
     model_name="sentence-transformers/all-MiniLM-L6-v2"
 )
+reranker = CrossEncoder('cross-encoder/ms-marco-MiniLM-L-6-v2') #classification model (act as grader and gives score)
+
 @tool
 def get_current_time():
     """Get the current real-time date and time."""
@@ -53,23 +59,42 @@ def search_wikipedia(query:str):
     
 
 @tool
-def search_knowledge_base(query: str):
+def search_knowledge_base(query: str, config: RunnableConfig):
     """
     Use this tool to search for information inside the uploaded PDF documents or text files.
     Input should be a specific search query related to the documents.
-    Returns the relevant text snippets from the files.
+    Returns the relevant text snippets from the files using a Two-Stage Advanced RAG pipeline.
     """
-    print(f"DEBUG: Searching Knowledge Base for: '{query}'")
+    user_id = config.get("configurable", {}).get("user_id")
+    print(f"DEBUG: Searching Knowledge Base for: '{query}',user:{user_id}")
     try:
         vector_db = QdrantVectorStore.from_existing_collection(
             embedding=embedding_model,
             url="http://localhost:6333",
             collection_name="learning-rag"
         )
-        results = vector_db.similarity_search(query, k=3)
-        if not results:
+        user_filter = models.Filter(
+            must=[
+                models.FieldCondition(
+                    key="metadata.user_id",
+                    match=models.MatchValue(value=user_id)
+                )
+            ]
+        )
+        initial_results = vector_db.similarity_search(query, k=15,filter=user_filter)
+        if not initial_results:
             return "No relevant information found in the documents."
-        context = "\n\n".join([f"Snippet: {res.page_content}" for res in results])
+        
+        print(f"DEBUG: Stage 1 found {len(initial_results)} snippets. Re-ranking...")
+
+        query_doc_pairs = [[query, doc.page_content] for doc in initial_results]
+        scores = reranker.predict(query_doc_pairs)
+        scored_docs = list(zip(initial_results, scores))
+        scored_docs.sort(key=lambda x: x[1], reverse=True)
+        top_3_docs = scored_docs[:3]
+
+        print(f"DEBUG: Top snippet score after re-ranking: {top_3_docs[0][1]:.2f}")
+        context = "\n\n".join([f"Snippet: {doc[0].page_content}" for doc in top_3_docs])
         return context
 
     except Exception as e:
@@ -77,3 +102,6 @@ def search_knowledge_base(query: str):
 
 
 tools_list = [get_current_time , web_search,search_wikipedia,search_knowledge_base]
+
+#we have not given user_id to llm as to protect from prompt injection attack.as llm fills out the parameter of search_knowledge_base when the tool is called.
+#so we use config={"configurable": {"user_id": user_id}}
